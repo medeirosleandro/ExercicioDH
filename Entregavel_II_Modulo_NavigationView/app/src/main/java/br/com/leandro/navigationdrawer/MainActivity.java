57688a171aee63e2270c7d74d56454762640e7f1
medeirosleandro <leandro.roberto.medeiros@gmail.com>
2018-08-01 18:02:39 -0300

    Modulo NavigationView - Entregavel II

---
+++ Entregavel_II_Modulo_NavigationView/app/src/main/java/br/com/leandro/navigationdrawer/MainActivity.java
@@ -0,0 +1,75 @@
+package br.com.leandro.navigationdrawer;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_drawer, R.string.close_drawer);
+        drawerLayout.addDrawerListener(toggle);
+
+        toggle.syncState();
+
+        navigationView = (NavigationView) findViewById(R.id.navView);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+
+    //Metodo para que pressionando o botão VOLTAR da barra inferior o NavigationDrawer seja recolhido
+    //caso esteja recolhido ele fecha o App
+
+    public void onBackPressed() {
+
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+    switch (item.getItemId()){
+
+        case R.id.menu_receitas:{
+            Toast.makeText(this,"ACIONANDO RECEITAS",Toast.LENGTH_SHORT).show();
+            break;
+        }
+
+        case R.id.menu_sobre:{
+            Toast.makeText(this,"ACIONANDO SOBRE",Toast.LENGTH_SHORT).show();
+            break;
+        }
+
+    }
+
+            return true;
+        }
+
+
+    }
